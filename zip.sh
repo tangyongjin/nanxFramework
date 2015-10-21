6abe85f665d2a7f72015fe806ddb4ba78dceea83
alex <tangyongjin@gmail.com>
2015-10-22 01:13:49 +0800

    zip.sh

--- zip.sh
+++ zip.sh
@@ -1,3 +1,10 @@
-zip -r standx.zip .   -x \*.git\*
-sudo cp standx.zip  /Library/WebServer/Documents/src_package/standx.zip
+echo "Zip source..."
+zip -rq standx.zip .   -x \*.git\*
+sudo cp standx.zip  /www3/src_package/standx.zip
 sudo rm standx.zip
+echo "Create nanx_template from standx..."
+mysqldump  --login-path=nanx   standx >standx.sql
+sed -i '1i DROP DATABASE IF EXISTS nanx_template;CREATE database `nanx_template` CHARACTER SET utf8 COLLATE utf8_general_ci;use nanx_template;' standx.sql
+mysql --login-path=nanx  <standx.sql
+sudo rm standx.sql
+echo "Done."
