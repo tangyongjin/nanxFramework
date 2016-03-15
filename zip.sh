c8baf108751c856f1b327230b78a4d7cc03cd8c7
tangyongjin <tangyongjin@gmail.com>
2016-03-15 19:36:39 +0800

    重新设置zip.sh

--- zip.sh
+++ zip.sh
@@ -3,7 +3,7 @@ zip -rq standx.zip .   -x \*.git\*
 sudo cp standx.zip  /var/www/html/nanx_web/src_package/standx.zip
 sudo rm standx.zip
 echo "Create nanx_template from standx..."
-mysqldump  -uroot -pdongman    standx >standx.sql
+mysqldump  -uroot -pdongman    cloud_standx >standx.sql
 sed -i '1i DROP DATABASE IF EXISTS nanx_template;CREATE database `nanx_template` CHARACTER SET utf8 COLLATE utf8_general_ci;use nanx_template;' standx.sql
 mysql  -uroot   -pdongman  <standx.sql
 sudo rm standx.sql
